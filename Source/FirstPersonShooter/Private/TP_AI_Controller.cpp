265678faaf7607ce88f7bc1ab48462328ad8ea2b
donfel <felixh2@gmail.com>
2019-05-28 17:49:55 +0300

    Adding Patrol AI

---
+++ Source/FirstPersonShooter/Private/TP_AI_Controller.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TP_AI_Controller.h"
+
+
+
+
---
+++ Source/FirstPersonShooter/Public/TP_AI_Controller.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TP_AI_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPERSONSHOOTER_API ATP_AI_Controller : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
